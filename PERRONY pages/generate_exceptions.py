78b252f685129718749f5129951b603b587a62a1
nedpet <jeremymtam@gmail.com>
2026-05-27 16:55:07 -0400

    updated generate_exceptions.py to accomodate p2p

--- PERRONY pages/generate_exceptions.py
+++ PERRONY pages/generate_exceptions.py
@@ -17,10 +17,14 @@ def run() -> tuple[str, str]:
     d = get_dict()
     exMap = "define ExMap [ "
     exFix = "define ExFix [ "
+    exMap2 = "define ExMap [ "
+    exFix2 = "define ExFix [ "
     for key in d:
         exMap += f'{space_word(key)} -> "EX_{key}" || WB _ WB .o. ' 
         exFix += f'"EX_{key}" -> {space_word(d[key])} || WB _ WB .o. ' 
-    return exMap[:-4] + "] ;", exFix[:-4] + "] ;"
+        exMap2 += f'{space_word(d[key])} -> "EX_{d[key]}" || WB _ WB .o. '
+        exFix2 += f'"EX_{d[key]}" -> {space_word(d[key])} || WB _ WB .o. '
+    return exMap[:-4] + "] ;", exFix[:-4] + "] ;", exMap2[:-4] + "] ;", exFix2[:-4] + "] ;"
 
 # define ExMap [ e t o t -> "etot" || WB _ WB .o. d e n t s -> "dents" || WB _ WB ] ;
 # define ExFix [ "etot" -> e t o || WB _ WB .o. "dents" -> d i || WB _ WB ] ;    
@@ -29,3 +33,5 @@ if __name__ == "__main__":
     result = run()
     print(result[0])
     print(result[1])
+    print(result[2])
+    print(result[3])
