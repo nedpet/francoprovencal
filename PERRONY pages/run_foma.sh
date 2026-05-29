7999a2642f17c881ff1fdc5c73aae09bb7580b52
Jeremy Tam <jeremym.tam@mail.utoronto.ca>
2026-05-28 23:55:47 -0400

    added further instructions to run_foma

--- PERRONY pages/run_foma.sh
+++ PERRONY pages/run_foma.sh
@@ -1,5 +1,12 @@
 # run foma within ubuntu, ex. ./run_foma.sh python_file.py
 
+# to create the venv, run these commands within home folder:
+# python3.13 -m venv foma-env
+# source ~/foma-env/bin/activate
+# pip install foma
+# pip install pympi-ling
+# deactivate
+
 #!/bin/bash
 source ~/foma-env/bin/activate
 python "$1"
